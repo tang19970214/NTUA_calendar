b72cb2087932e3789d9be9e6df55434937f937a6
coderken <jk0910948659@gmail.com>
2021-05-11 18:57:13 +0800

    add unit and location api

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,4 +1,4 @@
-import req from "./https";
+import req from "./https"
 
 // 全域 api 掛載於 Vue.prototype
 // 使用方式 this.$api
@@ -6,38 +6,39 @@ import req from "./https";
 const api = {
   // login
   GetToken(params) {
-    return req("get", `Login/GetClientToken`, params);
+    return req("get", `Login/GetClientToken`, params)
   },
   GetInfoByToken(params) {
-    return req("get", "Login/GetCalendarInfoByToken", params);
+    return req("get", "Login/GetCalendarInfoByToken", params)
   },
   GetNavigationBar(params) {
-    return req("get", "Permission/GetNavigationBar", params);
+    return req("get", "Permission/GetNavigationBar", params)
   },
   CheckTokenExpire(params) {
-    return req("get", "Login/GetExpireByToken", params);
-  },
-
-  // event type
-  GetEventType() {
-    return req("get", "Client/GetEventTypeList");
+    return req("get", "Login/GetExpireByToken", params)
   },
 
   // events
+  GetUnitListItem() {
+    return req("get", "Events/GetUnitListItem")
+  },
+  GetLocationListItem() {
+    return req("get", "Events/GetLocationListItem")
+  },
   GetEvents(params) {
-    return req("get", "Client/GetEventList", params);
+    return req("get", "Events/GetEvents", params)
   },
   GetEventsPage(params) {
-    return req("get", "CalendarEvent/GetClient", params);
+    return req("get", "CalendarEvent/GetClient", params)
   },
   GetEventById(params) {
-    return req("get", "Client/GetEventById/Id", params);
+    return req("get", "Client/GetEventById/Id", params)
   },
 
   //unit
   GetUnits() {
-    return req("get", "School/GetUnitList");
+    return req("get", "School/GetUnitList")
   },
-};
+}
 
-export default api;
+export default api
