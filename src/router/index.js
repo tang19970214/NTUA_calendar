ec4f275869b03d59af0712732c58ec7347591c9b
coderken <jk0910948659@gmail.com>
2021-05-13 17:18:23 +0800

    add calendar3 components

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,43 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import store from "../store";
-import { toCalendar } from "../api/utils";
+import Vue from "vue"
+import VueRouter from "vue-router"
+import store from "../store"
+import { toCalendar } from "../api/utils"
 
 // 防止重複點擊相同router噴error
-const originalPush = VueRouter.prototype.push;
+const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch((err) => err);
-};
+  return originalPush.call(this, location).catch((err) => err)
+}
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
     path: "/Calendar", //開頭大寫
     name: "Calendar", //開頭大寫
+    component: () => import("../views/Calendar3.vue"),
+    meta: {
+      title: "行事曆", //中文為主 若為英文開頭大寫
+      requireAuth: false, //表示是否登入驗證 false 時可省略
+      NoNeedHome: false, // 表示不需要父層模板 false 時可省略
+    },
+  },
+  {
+    path: "/Calendar2", //開頭大寫
+    name: "Calendar2", //開頭大寫
     component: () => import("../views/Calendar2.vue"),
     meta: {
       title: "行事曆", //中文為主 若為英文開頭大寫
+      requireAuth: false, //表示是否登入驗證 false 時可省略
+      NoNeedHome: false, // 表示不需要父層模板 false 時可省略
+    },
+  },
+  {
+    path: "/test", //開頭大寫
+    name: "test", //開頭大寫
+    component: () => import("../views/test.vue"),
+    meta: {
+      title: "測試行事曆", //中文為主 若為英文開頭大寫
       requireAuth: false, //表示是否登入驗證 false 時可省略
       NoNeedHome: false, // 表示不需要父層模板 false 時可省略
     },
@@ -62,16 +82,16 @@ const routes = [
     path: "*",
     redirect: "/Calendar",
   },
-];
+]
 
 const router = new VueRouter({
   mode: "hash",
   base: process.env.BASE_URL,
   routes,
   scrollBehavior(to, from, savedPosition) {
-    return { x: 0, y: 0 };
+    return { x: 0, y: 0 }
   },
-});
+})
 
 router.beforeEach((to, from, next) => {
   // console.log(store);
@@ -84,54 +104,54 @@ router.beforeEach((to, from, next) => {
   {
     if (!store.state.token) {
       if (window.localStorage.Token) {
-        store.commit("SAVE_TOKEN", window.localStorage.Token);
+        store.commit("SAVE_TOKEN", window.localStorage.Token)
         Vue.prototype.$api
           .CheckTokenExpire({ token: store.state.token })
           .then((res) => {
-            let flag = res.data.response;
+            let flag = res.data.response
             if (flag) {
               // console.log("token OOOO");
             } else {
               // console.log("token XXXXX");
-              toCalendar();
+              toCalendar()
             }
-          });
+          })
       } else {
-        console.log("LOCAL NO TOKEN");
+        console.log("LOCAL NO TOKEN")
       }
     }
     if (!store.state.tokenExpire) {
-      store.commit("SAVE_TOKEN_EXPIRE", window.localStorage.TokenExpire);
+      store.commit("SAVE_TOKEN_EXPIRE", window.localStorage.TokenExpire)
     }
 
     // 判斷該路由是否需要登入權限
     if (to.meta.requireAuth) {
       if (store.state.token && store.state.token != undefined) {
         // 通過vuex state獲取當前的token是否存在
-        next();
+        next()
       } else {
-        store.commit("SAVE_TOKEN", "");
-        store.commit("SAVE_TOKEN_EXPIRE", "");
-        window.localStorage.removeItem("user");
+        store.commit("SAVE_TOKEN", "")
+        store.commit("SAVE_TOKEN_EXPIRE", "")
+        window.localStorage.removeItem("user")
 
         Vue.prototype.$alertT.fire({
           icon: "info",
           title: `請先登入帳號`,
-        });
+        })
 
         next({
           path: "/Calendar",
           // query: { redirect: to.fullPath },
-        });
+        })
 
         // window.location.reload();
       }
     } else {
-      next();
+      next()
     }
   }
 
   // 檢查 router 設置
-});
+})
 
-export default router;
+export default router
