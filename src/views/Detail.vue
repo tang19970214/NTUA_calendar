d57e595254d2bf7ed2ba26fe20da425ab339de14
george <tang19970214@gmail.com>
2021-05-18 12:23:14 +0800

    upload

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="detailPage">
     {{detailList}}
   </div>
 </template>
@@ -26,3 +26,8 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.detailPage {
+}
+</style>
