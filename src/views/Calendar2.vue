5758c139ae6fcedcfa87567c33fa9b30cd8ea74d
coderken <jk0910948659@gmail.com>
2021-05-18 17:42:54 +0800

    fix id

--- src/views/Calendar2.vue
+++ src/views/Calendar2.vue
@@ -647,10 +647,10 @@ export default {
     /* 複製連結鈕 */
     copyHref(id) {
       console.log(id);
-      const oInput = document.createElement("input");
-      oInput.value = id; //賦值
-      document.body.appendChild(oInput);
-      oInput.select(); // 選擇物件
+      const insid = document.createElement("input");
+      insid.value = `id = "${id}"`; //賦值
+      document.body.appendChild(insid);
+      insid.select(); // 選擇物件
       document.execCommand("Copy"); // 執行瀏覽器複製命令
       // this.$router.push({ name: "Detail", query: { id: id } });
     },
