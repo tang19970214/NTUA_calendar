527c4b447806b9ce5268d51f2e9046c093c8cc4a
coderken <jk0910948659@gmail.com>
2021-05-19 17:09:34 +0800

    delete calendar3 component

--- src/router/index.js
+++ src/router/index.js
@@ -13,16 +13,6 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/Calendar3", //開頭大寫
-    name: "Calendar3", //開頭大寫
-    component: () => import("../views/Calendar3.vue"),
-    meta: {
-      title: "行事曆", //中文為主 若為英文開頭大寫
-      requireAuth: false, //表示是否登入驗證 false 時可省略
-      NoNeedHome: false, // 表示不需要父層模板 false 時可省略
-    },
-  },
-  {
     path: "/Calendar", //開頭大寫
     name: "Calendar", //開頭大寫
     component: () => import("../views/Calendar2.vue"),
