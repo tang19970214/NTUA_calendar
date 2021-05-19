549b6c4da3536b33932b2fd1a4eb56b712340724
george <tang19970214@gmail.com>
2021-05-19 17:21:41 +0800

    add share component

--- src/components/shareGroup.vue
+++ src/components/shareGroup.vue
@@ -4,64 +4,44 @@
       {{ item.title }}
     </div> -->
 
-    <ShareNetwork
-      class="shareGroupPage__block"
-      v-for="network in networks"
-      :network="network.network"
-      :key="network.network"
-      :style="{ backgroundColor: network.color }"
-      :url="sharing.url"
-      :title="sharing.title"
-      :description="sharing.description"
-      :quote="sharing.quote"
-      :hashtags="sharing.hashtags"
-      :twitterUser="sharing.twitterUser"
-    >
-      <i :class="network.icon"></i>
-    </ShareNetwork>
+    <div v-for="item in networks" :key="item.network">
+      <ShareNetwork class="shareGroupPage__block" :style="{ backgroundColor: item.color }" :network="item.network" :url="sharing.url" :title="sharing.title" :description="sharing.description" :quote="sharing.quote" :hashtags="sharing.hashtags" :twitterUser="sharing.twitterUser" v-if="item.name !== 'more'">
+        <i :class="item.icon"></i>
+      </ShareNetwork>
+
+      <div class="shareGroupPage__block" :style="{ backgroundColor: item.color }" @click="shareModal = true" v-else>
+        <a>
+          <i :class="item.icon"></i>
+        </a>
+      </div>
+    </div>
+
+    <el-dialog title="所有分享方式" :visible.sync="shareModal" width="40%">
+      <div class="shareGroupPage__modal">
+        <el-input v-model="shareList.keyword"></el-input>
+
+        <div class="shareGroupPage__modal--shareMethod">
+          <ShareNetwork v-for="item in allShareMethod" :key="item.network" class="shareGroupPage__block" :style="{ backgroundColor: item.color }" :network="item.network" :url="sharing.url" :title="sharing.title" :description="sharing.description" :quote="sharing.quote" :hashtags="sharing.hashtags" :twitterUser="sharing.twitterUser">
+            <i :class="item.icon"></i>
+            <span>{{ item.name }}</span>
+          </ShareNetwork>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="shareModal = false">關閉</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-// export default {
-//   data() {
-//     return {
-//       shareList: [
-//         {
-//           id: 1,
-//           icon: "",
-//           title: "twitter",
-//         },
-//         {
-//           id: 2,
-//           icon: "",
-//           title: "facebook",
-//         },
-//         {
-//           id: 3,
-//           icon: "",
-//           title: "pinter",
-//         },
-//         {
-//           id: 4,
-//           icon: "",
-//           title: "email",
-//         },
-//         {
-//           id: 5,
-//           icon: "",
-//           title: "more",
-//         },
-//       ],
-//     };
-//   },
-// };
-
 export default {
   data() {
     return {
       sharing: {
-        url: "https://news.vuejs.org/issues/180",
+        url:
+          /* process.env.VUE_APP_LINK_URL + */ "https://www.ntua.edu.tw/NTUACalendar/#" +
+          this.$route.fullPath,
         title:
           "Say hi to Vite! A brand new, extremely fast development setup for Vue.",
         description:
@@ -95,18 +75,38 @@ export default {
           icon: "far fah fa-lg fa-envelope",
           color: "#333333",
         },
+        {
+          network: "more",
+          name: "more",
+          icon: "far fah fa-lg fa-envelope",
+          color: "#45a147",
+        },
       ],
+      // modal
+      shareModal: false,
+      shareList: {
+        keyword: "",
+      },
+      allShareMethod: null,
     };
+  },
+  mounted() {
+    this.allShareMethod = this.$store.state.shareMethodList;
   },
 };
 </script>
 
 <style lang="scss">
 .shareGroupPage {
-  position: absolute;
+  position: fixed;
   left: 0;
-  top: 70px;
+  top: 15%;
   z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
   &__block {
     width: 28px;
     height: 28px;
@@ -115,18 +115,44 @@ export default {
     justify-content: center;
     align-items: center;
     padding: 8px;
+    color: white;
+    transition: 0.4s;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+    }
   }
-  .share-network-twitter {
-    color: #ffffff;
-  }
-  .share-network-facebook {
-    color: #ffffff;
-  }
-  .share-network-pinterest {
-    color: #ffffff;
-  }
-  .share-network-email {
-    color: #ffffff;
+
+  &__modal {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    &--shareMethod {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      flex-flow: wrap;
+
+      a {
+        display: inline;
+        align-items: center;
+        flex-direction: row;
+        margin: 0 4px 4px 0;
+
+        &:last-child {
+          margin: 0;
+        }
+
+        span {
+          padding-left: 8px;
+          white-space: nowrap;
+        }
+      }
+    }
   }
 }
 </style>
